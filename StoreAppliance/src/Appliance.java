03ed72c245aa208acde50c047538acf6f8d182b8
maritabarikhan <maritabarikhan@maritas-air>
2020-05-14 00:13:15 -0400

    Final commit

--- StoreAppliance/src/Appliance.java
+++ StoreAppliance/src/Appliance.java
@@ -6,7 +6,7 @@
 
 /**
  * 
- * @author  Marita Brichan and Mohona Mazumdar
+ * @author  Marita Brichan
  *<br>
  *<br> 
  *
--- StoreAppliance/src/Driver.java
+++ StoreAppliance/src/Driver.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 //-----------------------------------------------------
 /**
  * 
- * @author  Marita Brichan and Mohona Mazumdar
+ * @author  Marita Brichan
  *<br>
  *<br> 
  *
